80ad9c9602dff7104f24e021fbd1e44d150524c2
micahelrkytch <michaelr.kytch@gmail.com>
2015-09-07 12:55:45 -0700

    Bump version to 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject michaelrkytch/streamsum "0.1.1-SNAPSHOT"
+(defproject michaelrkytch/streamsum "0.1.1"
   :description "Configuration-driven summarization of event streams."
   :url "https://github.com/michaelrkytch/streamsum"
   :license {:name "Eclipse Public License"
